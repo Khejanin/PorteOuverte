5081bbdf8f7251af6680d80def4b61cb1597aba4
Khejanin <khejanin@gmail.com>
2018-03-09 17:25:11 +0100

    Added Game,Entity Class and static Width and Height constants.

---
+++ src/Game/Game.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Game;
+
+/**
+ *
+ * @author Khejanin
+ */
+public class Game{
+    
+    public static int TILE_WIDTH,TILE_HEIGHT;
+    
+    public Game(){
+        TILE_WIDTH = 1;
+    }
+    
+}
